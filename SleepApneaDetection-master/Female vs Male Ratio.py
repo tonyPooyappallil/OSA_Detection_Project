24bece8e76e641ed6bbf2b3aa010812397e9e117
Parth Patel <85731977+parthpatel973@users.noreply.github.com>
2025-05-21 10:45:18 +0800

    Update Female vs Male Ratio.py

--- SleepApneaDetection-master/Female vs Male Ratio.py
+++ SleepApneaDetection-master/Female vs Male Ratio.py
@@ -1,17 +1,18 @@
+# Imported required libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load predictions
+# To load predictions file
 df = pd.read_csv("D:/Dataset/patient_predictions.csv")
 
-# Normalize and convert sex values
-df["Sex"] = df["Sex"].astype(str).str.upper()  # convert m/f to M/F
+# To mormalize and convert into sex values
+df["Sex"] = df["Sex"].astype(str).str.upper()  
 df["Sex"] = df["Sex"].map({"M": "Male", "F": "Female"})
 
-# Drop any rows that didn't map correctly
+# To drop any rows that didn't map correctly
 df = df.dropna(subset=["Sex"])
 
-# Pie Chart
+# To create pie chart
 sex_counts = df["Sex"].value_counts()
 sex_counts.plot(kind="pie", autopct="%1.1f%%", startangle=140, labels=sex_counts.index, colors=["lightblue", "lightpink"])
 plt.title("Sex Distribution")
@@ -19,7 +20,7 @@ plt.ylabel("")
 plt.tight_layout()
 plt.show()
 
-# Bar Chart
+# To create bar Chart
 sex_counts.plot(kind="bar", color=["lightblue", "lightpink"])
 plt.title("Number of Male vs Female Patients")
 plt.xlabel("Sex")
