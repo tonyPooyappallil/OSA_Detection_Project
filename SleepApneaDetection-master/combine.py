5b92f0a8770019c564e5e0d056cc4817e7f815e2
Parth Patel <85731977+parthpatel973@users.noreply.github.com>
2025-05-21 10:42:59 +0800

    Update combine.py

--- SleepApneaDetection-master/combine.py
+++ SleepApneaDetection-master/combine.py
@@ -1,13 +1,14 @@
+# Imported required libraries
 import pandas as pd
 
-# Step 1: Load your CSV files
+# To load CSV files
 age_sex = pd.read_csv(r"D:\Dataset\age-sex.csv")
 ahi_data = pd.read_csv(r"D:\Dataset\ahi_results_with_severity.csv")
 
-# Step 2: Merge them using 'Record' column
+# To merge them using record column
 merged_data = pd.merge(age_sex, ahi_data, on="Record", how="inner")
 
-# Step 3: Save the result
+# To save the result
 merged_data.to_csv("D:/Dataset/merged_clinical_data.csv", index=False)
 
 print("Merged clinical data saved as merged_clinical_data.csv")
